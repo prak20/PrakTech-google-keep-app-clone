1e20d91a87acf00d46f8d35b5960306118915a77
Your Name <you@example.com>
2020-12-09 12:50:22 +0530

    Updates

---
+++ precache-manifest.64d0460cdda4fce2a7478d341a940647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9f83ee0b74f9f69505eec1f09a5d3b",
+    "url": "/Praktech-google-keep/index.html"
+  },
+  {
+    "revision": "fb0267edc39cc39e3dc5",
+    "url": "/Praktech-google-keep/static/css/main.c1c18ed8.chunk.css"
+  },
+  {
+    "revision": "322e55adfdec8faaad74",
+    "url": "/Praktech-google-keep/static/js/2.96ca816f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Praktech-google-keep/static/js/2.96ca816f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0267edc39cc39e3dc5",
+    "url": "/Praktech-google-keep/static/js/main.2deb4de4.chunk.js"
+  },
+  {
+    "revision": "0d1a926261fe6286aaff",
+    "url": "/Praktech-google-keep/static/js/runtime-main.03f2cd67.js"
+  },
+  {
+    "revision": "c6b0d8821ef012073b970069274491ba",
+    "url": "/Praktech-google-keep/static/media/logo.c6b0d882.png"
+  }
+]);
